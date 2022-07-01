3ae0f3682cc49a8f7f63f32e003154e3166b8774
sejesila <silaseje5@gmail.com>
2022-07-01 11:34:05 +0300

    Using '@test' annotation

--- tests/CartTest.php
+++ tests/CartTest.php
@@ -1,14 +1,29 @@
 <?php
 
+require 'Cart.php';
+
 class CartTest extends \PHPUnit\Framework\TestCase
 {
     public function testCorrectNetPriceIsReturned(){
-        require 'Cart.php';
+
         $cart = new Cart();
         $cart->price = 10;
         $netPrice = $cart->getNetPrice();
 
         $this->assertEquals(12,$netPrice);
+    }
+//    using test annotation
+    /** @test */
+
+    public function the_cart_value_can_be_changed_statically(){
+        Cart::$tax = 1.5;
+
+        $cart = new Cart();
+        $cart->price = 10;
+        $netPrice = $cart->getNetPrice();
+
+        $this->assertEquals(15,$netPrice);
+
     }
 
 }
